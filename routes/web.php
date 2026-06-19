4e264d2e4ec2342d2ca067172d581a890ab13b29
Md Farhan Rahman Anik <farhan.anik@gmail.com>
2026-06-19 20:18:28 +0200

    feat(catalog): add protected catalog routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Catalog\CategoryController;
+use App\Http\Controllers\Catalog\ProductController;
+use App\Http\Controllers\Catalog\SupplierController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,4 +27,70 @@ Route::middleware('auth')->group(function () {
 
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
+
+    Route::get('/categories', [CategoryController::class, 'index'])
+        ->middleware('permission:categories.view')
+        ->name('categories.index');
+    Route::get('/categories/create', [CategoryController::class, 'create'])
+        ->middleware('permission:categories.create')
+        ->name('categories.create');
+    Route::post('/categories', [CategoryController::class, 'store'])
+        ->middleware('permission:categories.create')
+        ->name('categories.store');
+    Route::get('/categories/{category}', [CategoryController::class, 'show'])
+        ->middleware('permission:categories.view')
+        ->name('categories.show');
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])
+        ->middleware('permission:categories.update')
+        ->name('categories.edit');
+    Route::match(['put', 'patch'], '/categories/{category}', [CategoryController::class, 'update'])
+        ->middleware('permission:categories.update')
+        ->name('categories.update');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])
+        ->middleware('permission:categories.delete')
+        ->name('categories.destroy');
+
+    Route::get('/suppliers', [SupplierController::class, 'index'])
+        ->middleware('permission:suppliers.view')
+        ->name('suppliers.index');
+    Route::get('/suppliers/create', [SupplierController::class, 'create'])
+        ->middleware('permission:suppliers.create')
+        ->name('suppliers.create');
+    Route::post('/suppliers', [SupplierController::class, 'store'])
+        ->middleware('permission:suppliers.create')
+        ->name('suppliers.store');
+    Route::get('/suppliers/{supplier}', [SupplierController::class, 'show'])
+        ->middleware('permission:suppliers.view')
+        ->name('suppliers.show');
+    Route::get('/suppliers/{supplier}/edit', [SupplierController::class, 'edit'])
+        ->middleware('permission:suppliers.update')
+        ->name('suppliers.edit');
+    Route::match(['put', 'patch'], '/suppliers/{supplier}', [SupplierController::class, 'update'])
+        ->middleware('permission:suppliers.update')
+        ->name('suppliers.update');
+    Route::delete('/suppliers/{supplier}', [SupplierController::class, 'destroy'])
+        ->middleware('permission:suppliers.delete')
+        ->name('suppliers.destroy');
+
+    Route::get('/products', [ProductController::class, 'index'])
+        ->middleware('permission:products.view')
+        ->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])
+        ->middleware('permission:products.create')
+        ->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])
+        ->middleware('permission:products.create')
+        ->name('products.store');
+    Route::get('/products/{product}', [ProductController::class, 'show'])
+        ->middleware('permission:products.view')
+        ->name('products.show');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])
+        ->middleware('permission:products.update')
+        ->name('products.edit');
+    Route::match(['put', 'patch'], '/products/{product}', [ProductController::class, 'update'])
+        ->middleware('permission:products.update')
+        ->name('products.update');
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])
+        ->middleware('permission:products.delete')
+        ->name('products.destroy');
 });
